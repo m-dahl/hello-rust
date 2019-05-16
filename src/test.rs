54a58627b7a7328dfbca1ca636d72d0ea274a129
Kristofer Bengtsson <kristofer@sekvensa.se>
2019-05-16 15:05:13 +0200

    Some more on predicates and actions

--- src/test.rs
+++ src/test.rs
@@ -1,5 +1,106 @@
 use uuid::Uuid;
 use serde::{Serialize, Deserialize, };
+use std::collections::HashMap;
+
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+enum Predicate {
+    AND(Vec<Predicate>),
+    OR(Vec<Predicate>),
+    Not(Box<Predicate>),
+    TRUE,
+    FALSE,
+    EQ(SPValue, SPValue),  // use SPValue::ID to fetch the value from the state
+    NEQ(SPValue, SPValue),
+    INDOMAIN(SPValue, Vec<SPValue>)
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+enum SPValue {
+    Bool(bool),
+    //byte(u8), deprecated
+    //char(char), deprecated
+    Float32(f32), 
+    Float64(f64),
+    Int8(i8), 
+    Uint8(u8),
+    Int16(i16), 
+    Uint16(u16),
+    Int32(i32), 
+    Uint32(u32),
+    Int64(i64), 
+    Uint64(u64),
+    String(String),
+    Time(u32),
+    Duration(u32),
+    ID(Uuid),     // use to also find the value in a state of variable with id
+    Array(Vec<SPValue>),
+    Map(HashMap<String, SPValue>)
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+struct SPState {
+    s: HashMap<String, SPValue>
+}
+
+//#[derive(Debug, PartialEq)]
+// Was hard to define something to store genereal functions
+// struct StateTransformation {
+//     tf: Fn(SPState) -> SPState,  // will be used by the runner
+//     to_action: Fn(SPState -> Vec<Action>)  // will be used for converting to a formal model
+// }
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+struct Action {
+    var: Uuid,
+    value: Compute
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+/// Used in actions to compute a new SPValue
+enum Compute {
+    Value(SPValue),
+    Get(Uuid),
+    TakeNext(Uuid, Vec<SPValue>), 
+    TakeBefore(Uuid, Vec<SPValue>),
+    Add(Box<Compute>, Box<Compute>),
+    Sub(Box<Compute>, Box<Compute>),
+    Join(Box<Compute>, Box<Compute>),
+}
+
+
+#[cfg(test)]
+mod testing_predicates {
+    use super::Predicate::*;
+    use super::*;
+
+    #[test]
+    fn testing() {
+        let test = AND(vec!(OR(vec!(TRUE))));
+        let kalle = SPAttributes::make(test).unwrap();
+
+        kalle.test();
+
+        let id = uuid::Uuid::new_v4();
+
+        let a = Action{var: id, value: Compute::Get(id)};
+
+        println!("{:?}", a)
+
+
+    }
+}
+
+
+
+
+
+///**************
+///  
+
+
 
 type SPJson = serde_json::Value;
 type SPJsonError = serde_json::Error;
@@ -21,47 +122,11 @@ impl SPAttributes {
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-enum Predicate {
-    AND(Vec<Predicate>),
-    OR(Vec<Predicate>),
-    Not(Box<Predicate>),
-    TRUE,
-    FALSE,
-    EQ(SPValue, SPValue),
-    NEQ(SPValue, SPValue),
-    INDOMAIN(SPValue, Vec<SPValue>)
-}
-
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-enum SPValue {
-    Bool(bool),
-    //byte(u8), deprecated
-    //char(char), deprecated
-    Float32(f32), 
-    Float64(f64),
-    Int8(i8), 
-    Uint8(u8),
-    Int16(i16), 
-    Uint16(u16),
-    Int32(i32), 
-    Uint32(u32),
-    Int64(i64), 
-    Uint64(u64),
-    String(String),
-    Time(u32),
-    Duration(u32),
-    ID(Uuid), 
-    Array(Vec<SPValue>),
-    Map(std::collections::HashMap<String, SPValue>)
-}
-
 struct SPObject {
     id: Uuid,
     name: String,
     attributes: SPAttributes,
 }
-
 
 trait IDAble {
     fn id(&self) -> Uuid;
@@ -71,9 +136,7 @@ trait IDAble {
 
 #[derive(Debug, PartialEq)]
 struct SPVariable {
-    id: Uuid,
-    name: String,
-    attributes: SPAttributes,
+    sp: SPObject,
     domain: Vec<SPValue>,
     init: SPValue
 }
